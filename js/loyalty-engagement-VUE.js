615ef384163a07f423f6b7ba5d47febfcbc4ffe4
seldii <seldaguzel@gmail.com>
2019-05-20 23:12:10 +0200

    vue.js attendance and loyalty page

--- js/loyalty-engagement-VUE.js
+++ js/loyalty-engagement-VUE.js
@@ -32,9 +32,10 @@ var vm = new Vue({
 
 
         },
-        tbl: document.querySelector("#glanceTable"),
-        tblSecond: document.getElementById("leastEngaged"),
-        tblThird: document.getElementById("mostEngaged"),
+
+        sortKey: "",
+        currentSortDir: "asc",
+
         urlSenate: 'https://api.propublica.org/congress/v1/113/senate/members.json',
         urlHouse: 'https://api.propublica.org/congress/v1/113/house/members.json',
 
@@ -42,7 +43,7 @@ var vm = new Vue({
     },
     methods: {
 
-        fetchedData: function () {
+        fetchData: function (x) {
             let opts = {
                 method: "GET",
                 headers: {
@@ -50,30 +51,34 @@ var vm = new Vue({
                     "X-API-Key": "OIg3ZQb6kfxzrPqdtMPvwGGFqW2UIRP8SP6ILTjR"
                 }
             };
-            fetch(url, opts)
-                .then(res => res.json())
-                .then(function (data) {
-                    this.members = data.results[0].members;
-                    this.loading = false;
+            fetch(x, opts)
+                .then(res => {
+                    return res.json()
+                })
+                .then(data => {
+                    this.members = data.results[0].members
+                    this.isLoading = false
+                    console.log(this.members)
+                    this.getPartyArr()
+                    this.loyaltyAndEngagement()
 
-                    getPartyArr(data)
-                    loyaltyAndEngagement(data)
-                    createTableEngandLoy()
 
                 })
-                .catch(console.error);
+                .catch(function (err) {
+                    console.log(err)
+                })
 
         },
 
 
 
-        loyaltyAndEngagement: function (data) {
+        loyaltyAndEngagement: function () {
 
             var mostLoyalGuys = [];
             var leastLoyalGuys = [];
-            var votesWpartyArr = data.map(x => x.votes_with_party_pct);
+            var votesWpartyArr = this.members.map(x => x.votes_with_party_pct);
             var reverseVotesWpartyArr = [];
-            var firstTenPercentNum = Math.round(data.length * 0.1);
+            var firstTenPercentNum = Math.round(this.members.length * 0.1);
 
             votesWpartyArr.sort(function (a, b) {
                 return a - b;
@@ -84,11 +89,11 @@ var vm = new Vue({
 
             var firstTenPerMostLoyal = reverseVotesWpartyArr.slice(0, firstTenPercentNum);
 
-            for (var j = 0; j < data.length; j++) {
+            for (var j = 0; j < this.members.length; j++) {
 
-                if (firstTenPerMostLoyal.includes(data[j].votes_with_party_pct)) {
+                if (firstTenPerMostLoyal.includes(this.members[j].votes_with_party_pct)) {
 
-                    mostLoyalGuys.push(data[j]);
+                    mostLoyalGuys.push(this.members[j]);
 
                 }
 
@@ -98,23 +103,23 @@ var vm = new Vue({
             var firstTenPerLeastLoyal = votesWpartyArr.slice(0, firstTenPercentNum);
 
 
-            for (var j = 0; j < data.length; j++) {
+            for (var j = 0; j < this.members.length; j++) {
 
-                if (firstTenPerLeastLoyal.includes(data[j].votes_with_party_pct)) {
+                if (firstTenPerLeastLoyal.includes(this.members[j].votes_with_party_pct)) {
 
-                    leastLoyalGuys.push(data[j]);
+                    leastLoyalGuys.push(this.members[j]);
 
                 }
 
             }
 
-            statistic.Total.MostLoyalGuys = mostLoyalGuys;
-            statistic.Total.LeastLoyalGuys = leastLoyalGuys;
+            this.statistic.Total.MostLoyalGuys = mostLoyalGuys;
+            this.statistic.Total.LeastLoyalGuys = leastLoyalGuys;
 
 
             var leastEngagedGuys = [];
             var mostEngagedGuys = [];
-            var missedVotesPerc = data.map(x => x.missed_votes_pct);
+            var missedVotesPerc = this.members.map(x => x.missed_votes_pct);
             var reverseMissedVotesPerc = [];
 
             missedVotesPerc.sort(function (a, b) {
@@ -127,11 +132,11 @@ var vm = new Vue({
 
             var firstTenPerLeastEngaged = reverseMissedVotesPerc.slice(0, firstTenPercentNum);
 
-            for (var j = 0; j < data.length; j++) {
+            for (var j = 0; j < this.members.length; j++) {
 
-                if (firstTenPerLeastEngaged.includes(data[j].missed_votes_pct)) {
+                if (firstTenPerLeastEngaged.includes(this.members[j].missed_votes_pct)) {
 
-                    leastEngagedGuys.push(data[j]);
+                    leastEngagedGuys.push(this.members[j]);
 
                 }
 
@@ -139,45 +144,47 @@ var vm = new Vue({
 
             var firstTenPerMostEngaged = missedVotesPerc.slice(0, firstTenPercentNum);
 
-            for (var j = 0; j < data.length; j++) {
+            for (var j = 0; j < this.members.length; j++) {
 
-                if (firstTenPerMostEngaged.includes(data[j].missed_votes_pct)) {
+                if (firstTenPerMostEngaged.includes(this.members[j].missed_votes_pct)) {
 
-                    mostEngagedGuys.push(data[j]);
+                    mostEngagedGuys.push(this.members[j]);
 
                 }
 
             }
 
-            statistic.Total.LeastEngagedGuys = leastEngagedGuys;
+            this.statistic.Total.LeastEngagedGuys = leastEngagedGuys;
 
-            statistic.Total.MostEngagedGuys = mostEngagedGuys;
+            this.statistic.Total.MostEngagedGuys = mostEngagedGuys;
 
 
 
         },
 
-        getPartyArr: function (data) {
+        getPartyArr: function () {
 
             var democratsArr = [];
             var republicanArr = [];
             var independentArr = [];
 
-            data.forEach(function (member) {
+            this.members.forEach(function (member) {
                 if (member.party.includes("D"))
                     democratsArr.push(member)
             })
 
-            data.forEach(function (member) {
+            this.members.forEach(function (member) {
                 if (member.party.includes("R"))
                     republicanArr.push(member)
             })
 
 
-            data.forEach(function (member) {
+            this.members.forEach(function (member) {
                 if (member.party.includes("I"))
                     independentArr.push(member)
             })
+
+
 
             function averageVoteWithParty(partyArr) {
                 var votesWpartyArr = partyArr.map(x => x.votes_with_party_pct);
@@ -190,58 +197,65 @@ var vm = new Vue({
 
             }
 
-            //Democrats
 
-            statistic.Democrats.NoOfReps = democratsArr.length;
 
-            statistic.Democrats.AveVotedWParty = averageVoteWithParty(democratsArr);
+            this.statistic.Democrats.NoOfReps = democratsArr.length;
 
 
 
-            //Republicans
-
-            statistic.Republicans.NoOfReps = republicanArr.length;
-
-            statistic.Republicans.AveVotedWParty = averageVoteWithParty(republicanArr);
+            this.statistic.Democrats.AveVotedWParty = averageVoteWithParty(democratsArr);
 
 
 
-            //Independents
 
-            statistic.Independents.NoOfReps = independentArr.length;
 
-            statistic.Independents.AveVotedWParty = averageVoteWithParty(independentArr);
+            this.statistic.Republicans.NoOfReps = republicanArr.length;
 
-            //Total
+            this.statistic.Republicans.AveVotedWParty = averageVoteWithParty(republicanArr);
 
-            statistic.Total.NoOfReps = data.length;
-            statistic.Total.AveVotedWParty = averageVoteWithParty(data);
 
+
+
+            this.statistic.Independents.NoOfReps = independentArr.length;
+
+            this.statistic.Independents.AveVotedWParty = averageVoteWithParty(independentArr);
+
+
+
+            this.statistic.Total.NoOfReps = this.members.length;
+            this.statistic.Total.AveVotedWParty = averageVoteWithParty(this.members);
+
+        },
+
+        sort: function (s) {
+            //if s == current sort, reverse
+            if (s === this.sortKey) {
+                this.currentSortDir = this.currentSortDir === 'asc' ? 'desc' : 'asc';
+            }
+            this.sortKey = s;
         }
     },
 
-    createTableEngandLoy: function () {
-
-        if (document.title.includes("Party Loyalty")) {
-            var leastLoyalMembers = statistic.Total.LeastLoyalGuys;
-            var mostLoyalMembers = statistic.Total.MostLoyalGuys;
-
-            generateTableLoyalty(leastLoyalMembers, tblSecondLoyalty)
-            generateTableLoyalty(mostLoyalMembers, tblThirdLoyalty)
+    computed: {
+        sortedMembers: function () {
+            return this.statistic.Total.LeastEngagedGuys.sort((a, b) => {
+                let modifier = 1;
+                if (this.currentSortDir === 'desc') modifier = -1;
+                if (a[this.sortKey] < b[this.sortKey]) return -1 * modifier;
+                if (a[this.sortKey] > b[this.sortKey]) return 1 * modifier;
+                return 0;
+            });
         }
+    },
 
 
-
-
-        if (document.title.includes("Attendance")) {
-
-            var leastEngagedMembers = statistic.Total.LeastEngagedGuys;
-            var mostEngagedMembers = statistic.Total.MostEngagedGuys;
-            generateTableEngagement(leastEngagedMembers, tblSecond)
-            generateTableEngagement(mostEngagedMembers, tblThird)
-
+    created: function () {
+        if (document.title.includes("Senate")) {
+            this.fetchData(this.urlSenate);
+        };
+        if (document.title.includes("House")) {
+            this.fetchData(this.urlHouse);
         }
-
-    }
+    },
 
 })
